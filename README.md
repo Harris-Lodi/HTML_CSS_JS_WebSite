d87bff680cd2b7acd8a9b1455de85c2b2e25f6ae
Harris-Lodi <harrislodi90s@gmail.com>
2020-10-05 22:30:13 -0400

    Website is done
    May come back to add more to the site, for the time being, this is for future reference only, do not publish this site ever!

--- README.md
+++ README.md
@@ -4,8 +4,12 @@
  
  This project is based on 'The Road' project from lashaNoz on Github, it's not meant to be public but to help me learn web dev as well as to use it as a reference guide for my future projects!<br />
  lashaNoz's project can be found here: https://github.com/lashaNoz/The-Road/tree/Final <br/>
+ lashaNoz's video can be found on his youtube channel 'CodeandCreate', link to video is here: https://bit.ly/2F4XCGX/<br />
 
  <b>Requirements</b><br />
  <ul>
  <li>A Local Host Server: I am using the 'Live Server' extension from Visual Studio Code/Codium to create a virtual server to view to site. You can use any virtual server tool to make your own server!</li>
  </ul><br />
+
+ <b>Notes: </b><br />
+ Project finished, can and may expand it in the future, in the mean time to be used as reference!<br />
--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+const container = document.querySelector('.container')
+
+/* code to control the open-navbar button to open navbar */
+document.querySelector('.open-navbar-icon').addEventListener('click', () => {
+    container.classList.add('change');
+});
+
+/* code to control the close-navbar button to close navbar */
+document.querySelector('.close-navbar-icon').addEventListener('click', () => {
+    container.classList.remove('change');
+});
+
+/* an array of color */
+const color = ['#6495ed', '#7fffd4', '#ffa07a', '#f08080', '#afeeee']
+
+/* initialize i to 0 */
+let i = 0;
+
+/* The following assigns for each of the navbar items a color from color array */
+Array.from(document.querySelectorAll('.nav-link')).forEach((item) => {
+    item.style.cssText = `background-color: ${color[i++]};`
+})
+
 /* select .navigation-button class to interact with, as an array object using for each loop */ 
 Array.from(document.querySelectorAll(".navigation-button")).forEach((item) => {
     item.onclick = () => { /* when the object of .navigation-button class is clicked ... */
